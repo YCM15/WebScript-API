9e1642bd2294081fecad27cb50f89f246b078a2c
YCM15 <yuniorcerrato26@yahoo.com>
2022-02-28 15:23:43 -0600

    modification route / message 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    return  res.send("WebScript API");
+    return  res.send("<h1>WebScript API</h1>");
   })
 
 module.exports = router;
